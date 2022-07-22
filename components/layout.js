b212d66829fc857c0295cf25e5c1725037fcfd32
Jef Mitchell <jef@project87.net>
2022-07-22 11:32:18 -0400

    updated layout and removed orphaned components

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,7 @@
-import Header from "./header";
-import Footer from "./footer";
-
 export default function Layout({ children }) {
   return (
     <>
-      <Header />
       <main>{children}</main>
-      <Footer />
     </>
   );
 }
