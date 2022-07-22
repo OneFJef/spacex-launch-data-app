5de4c42cbcebaa1af207bf1a8c1a05ea948b8443
Jef Mitchell <jef@project87.net>
2022-07-22 10:55:45 -0400

    styled content and refactored logic

--- pages/rocket/[id].js
+++ pages/rocket/[id].js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
+import dayjs from "dayjs";
+
 import Dropdown from "../../components/dropdown";
 
 export default function rockets({ rocketsData, upcomingData, pastData }) {
@@ -14,6 +16,7 @@ export default function rockets({ rocketsData, upcomingData, pastData }) {
   const upcomingFiltered = upcomingData.filter((obj) => {
     return obj.rocket === id;
   });
+  console.log(upcomingFiltered);
   const upcompingSorted = upcomingFiltered.sort((a, b) =>
     a.date_utc < b.date_utc ? 1 : -1
   );
@@ -22,6 +25,7 @@ export default function rockets({ rocketsData, upcomingData, pastData }) {
   const pastFiltered = pastData.filter((obj) => {
     return obj.rocket === id;
   });
+  console.log(pastFiltered);
   const pastSorted = pastFiltered.sort((a, b) =>
     a.date_utc < b.date_utc ? 1 : -1
   );
@@ -35,109 +39,152 @@ export default function rockets({ rocketsData, upcomingData, pastData }) {
       <div className="block m-8">
         <Dropdown />
       </div>
+      <h1 className="m-1 font-bold text-5xl text-white underline decoration-orange-400">
+        {rocketsFiltered[0].name}
+      </h1>
 
-      <div className="w-full md:w-5/6 lg:w-3/5 xl:w-1/2">
-        <div className="mt-8 p-2 rounded-t-lg bg-zinc-800 text-white w-1/3 text-center">
-          <p>Upcoming Launches</p>
-        </div>
-        {upcompingSorted.map((upcomingMapped) => (
-          <div
-            key={upcomingMapped.id}
-            className="flex items-center bg-zinc-800 border border-zinc-500 mb-4"
-          >
-            <div className="flex p-4 justify-center items-center h-auto w-40">
-              <ConditionalWrapper
-                condition={upcomingMapped.links.patch.small}
-                wrapper={(children) => (
-                  <Image
-                    alt={upcomingMapped.name}
-                    src={upcomingMapped.links.patch.small}
-                    width={150}
-                    height={150}
-                  />
-                )}
-              >
-                <Image
-                  alt={upcomingMapped.name}
-                  src={"/spacex_logo.jpeg"}
-                  width={125}
-                  height={125}
-                  className="rounded-full"
-                />
-              </ConditionalWrapper>
-            </div>
-            <div className="text-white">
-              <p className="my-2 pr-4">
-                Mission Name:{" "}
-                <span className="text-orange-400">{upcomingMapped.name}</span>
-              </p>
-              <p className="my-4 pr-4">
-                Launch Date:{" "}
-                <span className="text-orange-400">
-                  {upcomingMapped.date_utc}
-                </span>
-              </p>
-              <p className="my-2 pr-4">
-                Success/Failure: <span className="text-orange-400">TBD</span>
-              </p>
-            </div>
+      <ConditionalWrapper
+        condition={upcomingFiltered.length === 0 && pastFiltered.length === 0}
+        wrapper={(children) => (
+          <div className="text-white my-40 mx-10">
+            <p>
+              Sorry, there isn't any recorded launch data for{" "}
+              <span className="underline decoration-orange-400">
+                {rocketsFiltered[0].name}
+              </span>
+              .
+            </p>
           </div>
-        ))}
-
-        <div className="mt-8 p-2 rounded-t-lg bg-zinc-800 text-white w-1/3 text-center">
-          <p>Past Launches</p>
-        </div>
-
-        {pastSorted.map((pastMapped) => (
-          <div
-            key={pastMapped.id}
-            className="flex items-center bg-zinc-800 border border-zinc-500 mb-4"
+        )}
+      >
+        <div className="w-full md:w-5/6 lg:w-3/5 xl:w-1/2">
+          <ConditionalWrapper
+            condition={upcomingFiltered.length === 0}
+            wrapper={(children) => <div></div>}
           >
-            <div className="flex p-4 justify-center items-center h-auto w-40">
-              <ConditionalWrapper
-                condition={pastMapped.links.patch.small}
-                wrapper={(children) => (
-                  <Image
-                    alt={pastMapped.name}
-                    src={pastMapped.links.patch.small}
-                    width={150}
-                    height={150}
-                  />
-                )}
-              >
-                <Image
-                  alt={pastMapped.name}
-                  src={"/spacex_logo.jpeg"}
-                  width={125}
-                  height={125}
-                  className="rounded-full"
-                />
-              </ConditionalWrapper>
+            <div className="mt-8 ml-4 p-2 rounded-t-lg bg-zinc-800 text-white w-5/12 md:w-1/3 text-center">
+              <p>Upcoming Launches</p>
             </div>
-            <div className="text-white">
-              <p className="my-2 pr-4">
-                Mission Name:{" "}
-                <span className="text-orange-400">{pastMapped.name}</span>
-              </p>
-              <p className="my-2 pr-4">
-                Launch Date:{" "}
-                <span className="text-orange-400">{pastMapped.date_utc}</span>
-              </p>
-              <p className="my-2 pr-4">
-                Success/Failure:
+          </ConditionalWrapper>
+
+          {upcompingSorted.map((upcomingMapped) => (
+            <div
+              key={upcomingMapped.id}
+              className="flex items-center bg-zinc-800 border border-zinc-500 mb-4"
+            >
+              <div className="flex p-4 justify-center items-center h-auto w-40">
                 <ConditionalWrapper
-                  condition={pastMapped.success}
+                  condition={upcomingMapped.links.patch.small}
                   wrapper={(children) => (
-                    <span className="text-orange-400"> Success</span>
+                    <Image
+                      alt={upcomingMapped.name}
+                      src={upcomingMapped.links.patch.small}
+                      width={150}
+                      height={150}
+                    />
                   )}
                 >
-                  <span className="text-orange-400"> Failure - ________</span>
+                  <Image
+                    alt={upcomingMapped.name}
+                    src={"/spacex_logo.jpeg"}
+                    width={125}
+                    height={125}
+                    className="rounded-full"
+                  />
                 </ConditionalWrapper>
-              </p>
+              </div>
+              <div className="text-white">
+                <p className="my-2 pr-4">
+                  Mission Name:{" "}
+                  <span className="text-orange-400 inline-block">
+                    {upcomingMapped.name}
+                  </span>
+                </p>
+                <p className="my-4 pr-4">
+                  Launch Date:{" "}
+                  <span className="text-orange-400 inline-block">
+                    {dayjs(upcomingMapped.date_utc).format(
+                      "dddd, MMMM M, YYYY [at] HH:mm[Z]"
+                    )}
+                  </span>
+                </p>
+                <p className="my-2 pr-4">
+                  Success/Failure:{" "}
+                  <span className="text-orange-400 inline-block">TBD</span>
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+
+          <ConditionalWrapper
+            condition={pastFiltered.length === 0}
+            wrapper={(children) => <div></div>}
+          >
+            <div className="mt-8 ml-4 p-2 rounded-t-lg bg-zinc-800 text-white sm:w-5/12 md:w-1/3 text-center">
+              <p>Past Launches</p>
+            </div>
+          </ConditionalWrapper>
+          {pastSorted.map((pastMapped) => (
+            <div
+              key={pastMapped.id}
+              className="flex items-center bg-zinc-800 border border-zinc-500 mb-4"
+            >
+              <div className="flex p-4 justify-center items-center h-auto w-40">
+                <ConditionalWrapper
+                  condition={pastMapped.links.patch.small}
+                  wrapper={(children) => (
+                    <Image
+                      alt={pastMapped.name}
+                      src={pastMapped.links.patch.small}
+                      width={150}
+                      height={150}
+                    />
+                  )}
+                >
+                  <Image
+                    alt={pastMapped.name}
+                    src={"/spacex_logo.jpeg"}
+                    width={125}
+                    height={125}
+                    className="rounded-full"
+                  />
+                </ConditionalWrapper>
+              </div>
+              <div className="text-white">
+                <p className="my-2 pr-4">
+                  Mission Name:{" "}
+                  <span className="text-orange-400 inline-block">
+                    {pastMapped.name}
+                  </span>
+                </p>
+                <p className="my-2 pr-4">
+                  Launch Date:{" "}
+                  <span className="text-orange-400 inline-block">
+                    {dayjs(pastMapped.date_utc).format(
+                      "dddd, MMMM M, YYYY [at] HH:mm[Z]"
+                    )}
+                  </span>
+                </p>
+                <p className="my-2 pr-4">
+                  Success/Failure:{" "}
+                  <ConditionalWrapper
+                    condition={pastMapped.success}
+                    wrapper={(children) => (
+                      <span className="text-orange-400 inline-block">
+                        Success
+                      </span>
+                    )}
+                  >
+                    <span className="text-orange-400 inline-block">
+                      Failure - ________
+                    </span>
+                  </ConditionalWrapper>
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </ConditionalWrapper>
     </div>
   );
 }
